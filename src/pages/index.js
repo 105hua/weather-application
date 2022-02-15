00a90624036e78fedb5ac0337b2d3c0093978663
105hua <joshuajtudball2003@gmail.com>
2022-02-15 13:43:57 +0000

    Project Upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,278 @@
-import * as React from "react"
+import React, { useEffect } from "react"; // Import React because page won't work if it is not.
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Helmet from "react-helmet";
+import "./css/grid.css";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+// Page Style Variables.
+
+const titleStyle = {
+
+  textAlign: "center",
+  fontFamily: "'Nunito', sans-serif",
+  fontSize: "56px"
+
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const centreElement = {
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+const buttonStyle = {
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: "10px"
+
 }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+// Weather Card Style Variables
+
+const townName = {
+
+  textAlign: "center",
+  fontFamily: "'Nunito', sans-serif",
+  userSelect: "none"
+
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const cardEntryStyle = {
+
+  marginLeft: "15px",
+  marginTop: "0px",
+  marginBottom: "10px",
+  fontSize: "14px",
+  userSelect: "none"
+
 }
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+const conditionStyle = {
+
+  width: "128px",
+  height: "128px",
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "auto",
+  marginBottom: "auto"
+
 }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+// Page code.
 
-// markup
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+const IndexPage = () => { // Define the index page variable.
+
+  // Define body styles here.
+  var bodyStyle = document.body.style
+  bodyStyle.backgroundColor = "aliceblue";
+
+  function changeToCelcius(){
+
+    var response = document.getElementById("queryField").value;
+
+    if (response === ""){
+
+      alert("No response.");
+      return;
+
+    }
+
+    var apiUrl = "https://api.weatherapi.com/v1/current.json?key=06aa6a359bf1453a9c4195807221302&q=" + response;
+
+    fetch(apiUrl).then(response => response.json()).then(data => {
+
+      document.getElementById("temperature").innerHTML = "Temperature (C): " + data["current"]["temp_c"];
+      document.getElementById("feelslike").innerHTML = "Feels Like (C): " + data["current"]["feelslike_c"];
+
+    });
+
+    console.log("Changed to Celsius");
+
+  }
+
+  function changeToFahrenheit(){
+
+    var response = document.getElementById("queryField").value;
+
+    if (response === ""){
+
+      alert("No response.");
+      return;
+
+    }
+
+    var apiUrl = "https://api.weatherapi.com/v1/current.json?key=06aa6a359bf1453a9c4195807221302&q=" + response;
+
+    fetch(apiUrl).then(response => response.json()).then(data => {
+
+      document.getElementById("temperature").innerHTML = "Temperature (F): " + data["current"]["temp_f"];
+      document.getElementById("feelslike").innerHTML = "Feels Like (F): " + data["current"]["temp_f"];
+
+    });
+
+    console.log("Changed to Fahrenheit.");
+
+  }
+
+  // Functions go here.
+  function UpdateWeather(){
+
+    var response = document.getElementById("queryField").value;
+
+    if (response === ""){
+
+      alert("No response.");
+      return;
+
+    }
+
+    var apiUrl = "https://api.weatherapi.com/v1/current.json?key=06aa6a359bf1453a9c4195807221302&q=" + response;
+
+    try{ // HTTP Request Code.
+
+      fetch(apiUrl).then(response => response.json()).then(data => {
+        
+        document.getElementById("temperature").innerHTML = "Temperature (C): " + data["current"]["temp_c"];
+        document.getElementById("condition").innerHTML = "Condition: " + data["current"]["condition"]["text"];
+        document.getElementById("wind").innerHTML = "Wind (MPH/KPH): " + data["current"]["wind_mph"] + "/" + data["current"]["wind_kph"];
+        document.getElementById("windDirection").innerHTML = "Wind Direction: " + data["current"]["wind_dir"];
+        document.getElementById("humidity").innerHTML = "Humidity: " + data["current"]["humidity"] + "%";
+        document.getElementById("feelslike").innerHTML = "Feels Like (C): " + data["current"]["feelslike_c"];
+        document.getElementById("visibility").innerHTML = "Visibility (KM): " + data["current"]["vis_km"] + "km";
+        document.getElementById("pressure").innerHTML = "Pressure (MB/IN): " + data["current"]["pressure_mb"] + "/" + data["current"]["pressure_in"]
+        document.getElementById("uv").innerHTML = "UV: " + data["current"]["uv"] + "/10";
+        document.getElementById("localTime").innerHTML = "Local Time: " + data["location"]["localtime"];
+        document.getElementById("townName").innerHTML = data["location"]["name"];
+        document.getElementById("conditionImage").src = data["current"]["condition"]["icon"].replace("64x64", "128x128");
+
+      });
+
+    }catch(exception){
+
+      alert(exception);
+      return;
+
+    }
+
+  }
+
+  useEffect( () =>{
+
+    document.addEventListener("keydown", function(event){
+
+      if(event.which === 13){
+
+        UpdateWeather();
+
+      }
+
+    });
+
+  });
+
+  // HTML Stuff goes here.
+  return(
+
+    <>
+
+      <Helmet>
+
+        <title>Weather App</title>
+        
+      </Helmet>
+
+      <div id="fonts">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" />
+
+      </div>
+
+      <h1 style={ titleStyle }>Weather</h1>
+
+      <div id="textfield" style={centreElement}>
+
+        <TextField style={ centreElement } id="queryField" label="Query" variant="outlined" />
+
+      </div>
+
+      <div id="button" style={buttonStyle}>
+
+        <Button onClick={ UpdateWeather } variant="contained" >Get Weather</Button>
+
+      </div>
+
+      <div id="box" style={centreElement}>
+
+        <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          marginTop: '25px',
+          fontFamily: '"Nunito", sans-serif',
+          '& > :not(style)': {
+            m: 1,
+            width: 550,
+           height: 450,
+          },
+        }}
+      >
+        <Paper elevation={3}>
+
+        <h1 id="townName" style={ townName }>Town Name</h1>
+
+        <div class="weatherGrid">
+
+          <div class="areaOne">
+            
+            <h5 id="condition" style={ cardEntryStyle }>Condition: </h5>
+            <h5 id="temperature" style={ cardEntryStyle }>Temperature (C/F): </h5>
+            <h5 id="feelslike" style={ cardEntryStyle }>Feels Like (C/F): </h5>
+            <h5 id="wind" style={ cardEntryStyle }>Wind (MPH/KPH): </h5>
+            <h5 id="windDirection" style={ cardEntryStyle }>Wind Direction: </h5>
+            <h5 id="humidity" style={ cardEntryStyle }>Humidity: </h5>
+            <h5 id="visibility" style={ cardEntryStyle }>Visibility (KM): </h5>
+            <h5 id="pressure" style={ cardEntryStyle }>Pressure (MB/IN): </h5>
+            <h5 id="uv" style={ cardEntryStyle }>UV: </h5>
+            <h5 id="localTime" style={ cardEntryStyle }>Local Time: </h5>
+
+          </div>
+
+          <div class="areaTwo">
+
+              <img id="conditionImage" style={ conditionStyle } src="https://cdn.weatherapi.com/weather/128x128/day/116.png"/>
+
+          </div>
+
+        </div>
+
+        </Paper>
+      </Box>
+
+    </div>
+
+    <div style={ centreElement }>
+
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <Button onClick={ changeToCelcius }>Celsius</Button>
+        <Button onClick={ changeToFahrenheit }>Fahrenheit</Button>
+      </ButtonGroup>
+
+    </div>
+
+    </>
+
   )
+
 }
 
-export default IndexPage
+export default IndexPage // Export indexpage by default.
